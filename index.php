a5ce1cd65369ebff57e4767a6a0efa97823e3d2c
kentonquatman <kentonquatman@gmail.com>
2015-06-28 23:59:31 -0400

    Added Boilerplate Files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  $pageTitle = 'To Be Collected';
+  $bodyId = 'home';
+  include '_partials/head.php';
+  include '_partials/header.php';
+?>
+
+<section class="main">
+  
+</section>
+
+<?php
+  include '_partials/footer.php';
+  include '_partials/foot.php';
+?>
