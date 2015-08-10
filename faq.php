43d879bf321e0a07f0866bde3f84fb63298adc74
kentonquatman <kentonquatman@gmail.com>
2015-08-10 00:11:00 -0400

    Started FAQ Page

---
+++ faq.php
@@ -0,0 +1,42 @@
+<?php
+  $pageTitle = 'To Be Collected';
+  $bodyId = 'page';
+  include '_partials/head.php';
+  include '_partials/header.php';
+?>
+
+<section class="main">
+  <div class="main__top">
+    <div class="inner">
+      <h1>frequently asked questions</h1>
+    </div>
+  </div>
+  <div class="inner">
+    <article>
+      <h5>DELIVERY TIMES AND COST?</h5>
+      <p>We try and ship in stock orders the next working day and DHL Express usually takes 1-3 days to main cities.</p>
+      <h5>DO YOU SHIP WORLDWIDE?</h5>
+      <p>We ship To Be Collected goods all over the world to most destinations everyday from the U.S. with DHL. You can order with confidence, safe in the knowledge that we'll do our best to get your goods to you as quick as we can. It takes just a few days to deliver your order, wherever you are, however we don't like to guarantee exact delivery times due to unexpected delays that could occur with customs etc.</p>
+      <p>If your shipping destination is not available, please contact us.</p>
+      <h5>HOW DO I PAY FOR MY ORDER?</h5>
+      <p>Prices on our website are currently charged in U.S. dollars. We accept credit card payments from Visa, MasterCard, and Discover through our secure site.</p>
+      <h5>I HAVE JUST PLACED MY ORDER, WHAT HAPPENS NEXT?</h5>
+      <p>We will start processing your order immediately. As long as the goods are in stock, they will be packed up carefully within five working days and sent off. Once we have received your order it's not possible to change or cancel it. If you have made a mistake with your shipping address it's not always possible to change it in time but please contact us immediately to see what we can do.</p>
+      <h5>CAN I TRACK MY ORDER?</h5>
+      <p>As soon as your order has shipped, we'll email you the tracking number.</p>
+      <h5>DO I HAVE TO PAY TAX OR IMPORT FEES?</h5>
+      <p>If your delivery address is within the U.S., taxes will be calculated into your total price. Product shipping to all other destinations do not include taxes but you may have to pay import fees and tax on arrival. Import fees vary widely across the world, so please contact your customs office for further details.</p>
+      <h5>WHAT IS THE RETURN POLICY?</h5>
+      <p>We want you to be happy with your new To Be Collected products but if you unpack the goods and find that it's not quite right for you, please email us immediately at returns@Tobecollected.com for details on how to return your item.</p>
+      <p>All items will need to be sent back to hard graft within 7 days of your receiving the goods (it needs to be postmarked within these 7 days) to qualify for a refund or store credit. Returns sent after this 7 day period will not be accepted. Return postage costs are the customer's responsibility.</p>
+      <p>We can only accept your return if the goods are absolutely unused and in the original condition. In turn we would never send out used goods to you. When you purchase from hard graft we guarantee that you'll get a brand new product. You will be the first owner of a brand new case, bag or pair of shoes. You get to wear it in and make it yours.</p>
+      <p>We are sure you understand that if we receive a used case from you (even if it is used for only one day) it will show and due to our strict guidelines we won't be able to accept the return. So in the unlikely event that you decide that the product is not for you after using and testing it, we recommend to pass it on to a friend or try and sell it privately (hard graft goods tend to have a very good resale value).</p>
+      <p>We are also not able to directly exchange goods so if you decide you would like a different product you will need to return or cancel your order and place a new one. In the unlikely event that you are not happy with your goods, please email us immediately as we will need to authorize the return. Your return must be dispatched within seven working days from receiving the delivery. Return requests will not be accepted outside of this period or without the supplied return address label. Once the goods are returned to us in the original and unused condition we will process a refund for the goods. If the return is despatched after seven working days we will issue a credit only at our discretion. If you send back used goods they will be sent back to you at your expense. Shipping costs and import fees are not refundable. Please note that returns from outside the U.S. that are shipped duty unpaid will be refused until any associated fees are paid by the sender.</p>
+    </article>
+  </div>
+</section>
+
+<?php
+  include '_partials/footer.php';
+  include '_partials/foot.php';
+?>
