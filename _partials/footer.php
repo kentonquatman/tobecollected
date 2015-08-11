f478f17740772d25081adb2a4ac13bf7348ce34b
kentonquatman <kentonquatman@gmail.com>
2015-08-10 23:03:40 -0400

    Bottom Section

--- _partials/footer.php
+++ _partials/footer.php
@@ -1,3 +1,16 @@
+<section class="bottom">
+  <div class="inner">
+    <aside>
+      <h2>not finding what you're looking for?</h2>
+      <p>Contact us with your request. We will do our best  to recommend pieces that either match or come close to what you are seeking. We can't always guarantee success, but with our network of artisans, we have a deep resource of options for you to select from.</p>
+    </aside>
+    <div class="bottom__buttons">
+      <a class="button" href="contact.php">Contact Us</a>
+      <a class="button" href="gift-card.php">Gift Cards</a>
+    </div>
+  </div>
+</section>
+
 <footer>
   
 </footer>
