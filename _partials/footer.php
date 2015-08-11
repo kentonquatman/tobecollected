c21ad42d74db7ce793518eec715f17e3ed699fa9
kentonquatman <kentonquatman@gmail.com>
2015-08-11 00:23:17 -0400

    Footer Styles

--- _partials/footer.php
+++ _partials/footer.php
@@ -11,6 +11,45 @@
   </div>
 </section>
 
-<footer>
-  
+<footer class="site-footer">
+  <div class="inner">
+    <nav class="site-footer__nav">
+      <div class="nav-group">
+        <h5>Contact Us</h5>
+        <p>800.555.1212</p>
+        <p><a href="mailto:info@tobecollected.com">info@tobecollected.com</a></p>
+        <address>
+          To Be Collected<br>
+          321 S. Jefferson Street<br>
+          Floor 4.<br>
+          Chicago, Illinois 60661
+        </address>
+      </div>
+      <div class="nav-group">
+        <h5>Customer Service</h5>
+        <ul>
+          <li><a href="page.php">Terms and Conditions</a></li>
+          <li><a href="page.php">Privacy Policy</a></li>
+          <li><a href="page.php">Shipping and Returns</a></li>
+          <li><a href="faq.php">FAQ's</a></li>
+        </ul>
+      </div>
+      <div class="nav-group">
+        <h5>My Account</h5>
+        <ul>
+          <li><a href="page.php">Login/Register</a></li>
+          <li><a href="page.php">View Cart</a></li>
+          <li><a href="page.php">Track My Order</a></li>
+        </ul>
+      </div>
+      <span class="site-footer__share-button">
+        <a href="#">Share This Page</a>
+      </span>
+    </nav>
+    <ul class="site-footer__social-links">
+      <li class="facebook"><a href="#">Facebook</a></li>
+      <li class="pinterest"><a href="#">Pinterest</a></li>
+    </ul>
+    <p class="site-footer__copyright">&copy; To Be Collected LLC</p>
+  </div>
 </footer>
