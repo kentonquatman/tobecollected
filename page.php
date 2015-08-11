3af2a459061af345f4981cf36d5322a805287e39
kentonquatman <kentonquatman@gmail.com>
2015-08-11 00:39:23 -0400

    Type Style Updates

---
+++ page.php
@@ -0,0 +1,49 @@
+<?php
+  $pageTitle = 'To Be Collected';
+  $bodyId = 'page';
+  include '_partials/head.php';
+  include '_partials/header.php';
+?>
+
+<section class="main">
+  <div class="main__top">
+    <div class="inner">
+      <h1>template page</h1>
+    </div>
+  </div>
+  <div class="inner">
+    <article>
+      <h1>H1 Heading</h1>
+      <p>Nunc porta imperdiet lorem varius ornare consequat quisque condimentum. Mauris amet id vulputate est eleifend fusce. Volutpat et elementum ac amet justo. Amet pretium sed aliquam pellentesque duis pulvinar ullamcorper lorem purus. Nec habitant cursus tortor ac vestibulum. Pretium imperdiet tempus dolor cras non ac turpis sem id. Ante varius rhoncus eros praesent rutrum lacus est aliquam. Dapibus nulla neque arcu sollicitudin nullam fames tortor. Aliquam accumsan eget senectus semper auctor id tempus vivamus enim.</p>
+      <h2>H2 Heading</h2>
+      <p>Maximus quam nulla finibus dapibus vivamus tincidunt bibendum turpis convallis. Lacinia orci fames elit duis fringilla. Feugiat blandit magna pharetra lorem a pellentesque dapibus ex amet. In elementum cras ipsum nunc integer. Ornare ut in dictum nunc duis dapibus eu lorem convallis. Felis enim fames nunc metus pellentesque. Ex gravida netus urna interdum fames vel elit velit. Dignissim ex viverra cursus dapibus cras.</p>
+      <h3>H3 Heading</h3>
+      <p>Pulvinar fames nunc sit nam vulputate. Lobortis est duis metus orci placerat. Senectus pellentesque dui nullam in neque. Sit senectus suscipit curabitur duis justo sollicitudin. Imperdiet lorem vulputate ullamcorper donec lectus quis magna sem fringilla.</p>
+      <ul>
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+      </ul>
+      <p>Volutpat lorem ante augue sollicitudin finibus nulla neque. Aliquet eu ornare nullam ut varius accumsan consequat risus. Lobortis sed senectus eros fusce ipsum nulla tellus leo quam. Augue elit enim cras turpis auctor mi lectus. Volutpat interdum augue lorem eros mollis purus vitae a. Gravida amet fermentum arcu dapibus pellentesque. Porttitor turpis a sem fringilla viverra phasellus nec finibus diam. Consectetur luctus at urna augue habitant integer lacus venenatis massa.</p>
+      <ol>
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+      </ol>
+      <p>Aliquet risus mauris mi ornare non massa lectus laoreet. Aliquet augue eu semper quis vitae eleifend porttitor cursus. Est pharetra mattis phasellus quam vulputate. Proin et urna consectetur diam cursus pulvinar porttitor risus. Est fames ipsum lobortis fermentum felis sem adipiscing. Quis fames erat mauris et ullamcorper mi turpis sem. Interdum ut elit imperdiet laoreet justo mollis luctus bibendum lacinia.</p>
+      <h4>H4 Heading</h4>
+      <p>Nunc mi venenatis tristique enim in a suspendisse mollis ornare. Eleifend senectus ac magna molestie lacinia. In fames magna elit nisi eu accumsan sapien pulvinar ornare. Rhoncus nec cras ullamcorper scelerisque sollicitudin tristique eros sit. Fermentum commodo habitant cras quam diam. Lacus massa mi lorem morbi cursus eget. Suspendisse nulla aliquam nunc laoreet tellus elit accumsan tortor metus.</p>
+      <blockquote>Pull quote that can be placed in a block of text</blockquote>
+      <p>Quam orci senectus aliquet ullamcorper mollis mauris. Vivamus efficitur enim facilisis tellus fermentum mauris cursus arcu. Dui proin sodales nec facilisis consequat nunc odio pretium convallis. Enim blandit laoreet tellus cursus lacus volutpat sodales curabitur. Dignissim venenatis pharetra pellentesque orci pulvinar senectus. Finibus rutrum dui est habitant senectus maximus consectetur et pellentesque. Iaculis enim nullam integer sodales lorem erat cras porttitor. In fames hendrerit arcu tristique turpis non malesuada.</p>
+      <h5>H5 Heading</h5>
+      <p>Felis est finibus lorem nullam orci fames rutrum. Mi congue tempus ipsum euismod nunc amet pellentesque. Elit viverra hendrerit congue metus morbi dolor. Efficitur morbi consectetur eu et duis sodales pellentesque. Elit elementum lectus dignissim urna fringilla eros a aliquet netus. Eros lorem vitae eu magna mauris. Lacinia adipiscing vestibulum et accumsan dapibus congue. Vestibulum vel eleifend mauris nullam aliquam nunc a arcu condimentum. Tortor rhoncus justo vel ullamcorper est nisi adipiscing curabitur nullam.</p>
+      <h6>H6 Heading</h6>
+      <p>Quam orci senectus aliquet ullamcorper mollis mauris. Vivamus efficitur enim facilisis tellus fermentum mauris cursus arcu. Dui proin sodales nec facilisis consequat nunc odio pretium convallis. Enim blandit laoreet tellus cursus lacus volutpat sodales curabitur. Dignissim venenatis pharetra pellentesque orci pulvinar senectus. Finibus rutrum dui est habitant senectus maximus consectetur et pellentesque. Iaculis enim nullam integer sodales lorem erat cras porttitor. In fames hendrerit arcu tristique turpis non malesuada.</p>
+    </article>
+  </div>
+</section>
+
+<?php
+  include '_partials/footer.php';
+  include '_partials/foot.php';
+?>
