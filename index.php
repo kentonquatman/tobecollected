374785d9702d69fb0f6baba6702d8c71dbd7fa72
kentonquatman <kentonquatman@gmail.com>
2015-08-19 02:37:52 -0400

    Started Home Page Styles

--- index.php
+++ index.php
@@ -1,12 +1,72 @@
 <?php
   $pageTitle = 'To Be Collected';
-  $bodyId = 'page';
+  $bodyId = 'home';
   include '_partials/head.php';
   include '_partials/header.php';
 ?>
 
-<section class="main">
-
+<section class="product-list">
+  <div class="inner">
+    <article>
+      <a href="product.php">
+        <figure>
+          <img src="http://placehold.it/360x240.jpg" alt="photo">
+        </figure>
+        <h6>Product Name <span class="spacer">|</span> $100</h6>
+      </a>
+    </article>
+    <article>
+      <a href="product.php">
+        <figure>
+          <img src="http://placehold.it/360x240.jpg" alt="photo">
+        </figure>
+        <h6>Product Name <span class="spacer">|</span> $100</h6>
+      </a>
+    </article>
+    <article>
+      <a href="product.php">
+        <figure>
+          <img src="http://placehold.it/360x240.jpg" alt="photo">
+        </figure>
+        <h6>Product Name <span class="spacer">|</span> $100</h6>
+      </a>
+    </article>
+    <article>
+      <a href="product.php">
+        <figure>
+          <img src="http://placehold.it/360x240.jpg" alt="photo">
+        </figure>
+        <h6>Product Name <span class="spacer">|</span> $100</h6>
+      </a>
+    </article>
+    <article>
+      <a href="product.php">
+        <figure>
+          <img src="http://placehold.it/360x240.jpg" alt="photo">
+        </figure>
+        <h6>Product Name <span class="spacer">|</span> $100</h6>
+      </a>
+    </article>
+    <article>
+      <a href="product.php">
+        <figure>
+          <img src="http://placehold.it/360x240.jpg" alt="photo">
+        </figure>
+        <h6>Product Name <span class="spacer">|</span> $100</h6>
+      </a>
+    </article>
+  </div>
+  <div class="pagination bottom">
+    <div class="pagination__links">
+      <a href="#" class="prev">&lt;</a>
+      <ol>
+        <li class="active"><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+      </ol>
+      <a href="#" class="next">&gt;</a>
+    </div>
+  </div>
 </section>
 
 <?php
